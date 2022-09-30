66b79544ec631ba46ef9f1d3da90f79ffdbaed34
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-09-30 17:28:11 +0300

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
     return (
         <div className=' flex-row h-screen bg-no-repeat m-0 pt-5'>
-            <p className='flex ml-auto mr-auto mt-auto mb-5 justify-center items-center text-6xl text-gray-600 w-full'>Sorry, page not found :( <Link to="/">Please go to main page</Link></p>
+            <p className='flex ml-auto mr-auto mt-auto mb-5 justify-center items-center text-6xl text-gray-600 w-full'>Sorry, page not found :( <Link className=" text-red-700" to="/">Click here to go to the main page</Link></p>
         </div>
     )
 };
